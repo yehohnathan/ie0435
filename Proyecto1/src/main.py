e01d3a2ea96f69a544fd3c5955dadf448feb0a0f
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-08-26 23:51:39 -0600

    Actualización de main
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

--- Proyecto1/src/main.py
+++ Proyecto1/src/main.py
@@ -1,18 +1,23 @@
 # ---------------- # Se importan las librerías necesarias # ----------------- #
 import pandas as pd                     # Manejo del CSV
-from Proyecto1.src.KMedias import SistemasAgrupamiento
+from KMedias import KMedias
+from MarkdownToCSV import MarkdownToCSV
 # Para evitar el error de detección de nucleos de mi computadora
 import os   # Evita la detección de nucleos
 os.environ['LOKY_MAX_CPU_COUNT'] = '2'
 
-# -------------------------- # Se importa el CSV # -------------------------- #
-name_csv = ("../Datos_Abiertos_ARESEP" +
-            "_Indicadores_de_continuidad_por_empresa_2022.csv")
+# --------------------- # Convierte de Markdown a CSV # --------------------- #
+# Prueba de pasar de AnexoA.md a AnexoA.csv
+name_md = ("../Datos/AnexoA.md")
+md_to_csv = MarkdownToCSV(name_md)
+
+# Se carga la dirección del csv
+name_csv = md_to_csv.convert()
 
 # ---------------------- # Análisis y limpiza del CSV # --------------------- #
 # Se carga el CSV en un DataFrame:
 aresep_df = pd.read_csv(name_csv)
-aresep_df = aresep_df.loc[:, ['DPIR', 'FPI', 'Abonados Por Circuito']]
+aresep_df = aresep_df.loc[:, ['DPIR', 'FPI', 'Abonados']]
 
 # Eliminar filas con valores NaN
 aresep_df = aresep_df.dropna()
@@ -26,10 +31,10 @@ print("\n==================== Información del DataFrame: ====================")
 aresep_df.info()
 
 # --- # Prueba de K-Means # --- #
-kmedias = SistemasAgrupamiento()
+kmedias = KMedias()
 kmedias.setDataFrame(aresep_df)
-kmedias.metodo_elbow(['Abonados Por Circuito', 'DPIR', 'FPI'])
-kmedias.grafica_KMeans('Abonados Por Circuito', 'DPIR',
+kmedias.metodo_elbow(['Abonados', 'DPIR', 'FPI'])
+kmedias.grafica_KMeans('Abonados', 'DPIR',
                        'Abonados', 'DPIR', k_clusters=4)
-kmedias.grafica_KMeans_3D('Abonados Por Circuito', 'DPIR', 'FPI',
+kmedias.grafica_KMeans_3D('Abonados', 'DPIR', 'FPI',
                           'Abonados', 'DPIR', 'FPI', k_clusters=4)
