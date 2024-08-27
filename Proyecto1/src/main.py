8bb5457dcf5cd91f7445577a2b840796e8ba8b4b
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-08-27 14:38:17 -0600

    Se probó el sistema DBSCAN con exito, falta testearlo
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

--- Proyecto1/src/main.py
+++ Proyecto1/src/main.py
@@ -1,17 +1,19 @@
 # ---------------- # Se importan las librerías necesarias # ----------------- #
 import pandas as pd                     # Manejo del CSV
 from KMedias import KMedias
+from DBSCAN_3D import DBSCAN_3D
 from MarkdownToCSV import MarkdownToCSV
 # Para evitar el error de detección de nucleos de mi computadora
 import os   # Evita la detección de nucleos
 os.environ['LOKY_MAX_CPU_COUNT'] = '2'
 
 # --------------------- # Convierte de Markdown a CSV # --------------------- #
-# Prueba de pasar de AnexoA.md a AnexoA.csv
-name_md = ("../Datos/AnexoA.md")
-md_to_csv = MarkdownToCSV(name_md)
+# Determinar la ruta absoluta del archivo markdown
+current_dir = os.path.dirname(os.path.abspath(__file__))
+name_md = os.path.join(current_dir, "..", "Datos", "AnexoA.md")
 
-# Se carga la dirección del csv
+# Convertir de Markdown a CSV
+md_to_csv = MarkdownToCSV(name_md)
 name_csv = md_to_csv.convert()
 
 # ---------------------- # Análisis y limpiza del CSV # --------------------- #
@@ -33,8 +35,12 @@ aresep_df.info()
 # --- # Prueba de K-Means # --- #
 kmedias = KMedias()
 kmedias.setDataFrame(aresep_df)
-kmedias.metodo_elbow(['Abonados', 'DPIR', 'FPI'])
-kmedias.grafica_KMeans('Abonados', 'DPIR',
-                       'Abonados', 'DPIR', k_clusters=4)
-kmedias.grafica_KMeans_3D('Abonados', 'DPIR', 'FPI',
-                          'Abonados', 'DPIR', 'FPI', k_clusters=4)
+kmedias.metodo_elbow('Abonados', 'DPIR', 'FPI')
+kmedias.grafica_KMeans('Abonados', 'DPIR', k_clusters=4)
+kmedias.grafica_KMeans_3D('Abonados', 'DPIR', 'FPI', k_clusters=4,
+                          xlabel='Abonados por circuito')
+
+# --- # Prueba de DBSCAN terna # --- #
+dbscan = DBSCAN_3D()
+dbscan.setDataFrame(aresep_df)
+dbscan.grafica_DBSCAN_3D('Abonados', 'DPIR', 'FPI')
